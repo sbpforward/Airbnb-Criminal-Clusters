b52589be6865fcaf5dacf352ae55971bd93cfc4a
sbpforward <sbpforward@gmail.com>
2019-10-16 09:09:54 -0600

    clean + standardized complete

--- src/clean_listng.py
+++ src/clean_listng.py
@@ -71,7 +71,22 @@ def NaN_to_None(df, cols):
     '''
     for col in cols:
         df[col].fillna('none', inplace=True)
-        df.head()
+    return df
+
+def NaN_to_zero(df):
+    '''
+    Covert NaN's to 'none'.
+    
+    Parameters
+    ----------
+    df: 
+    cols: 
+
+    Returns
+    ----------
+    df: 
+    '''
+    df['review_scores_rating'].fillna(value=0, inplace=True)
     return df
 
 def host_in_Denver(df):
@@ -196,7 +211,7 @@ def current_license(df):
     df['current_license'] = df['license'].map(lambda x: 1.0 if x in current_lst else 0.0) 
     return df
 
-def drop_cols(df, cols):
+def drop_cols(df):
     '''
     Desc.
     
@@ -209,11 +224,27 @@ def drop_cols(df, cols):
     ----------
     df: 
     '''
-    df.drop(columns=['host_location', 'host_is_superhost', 'city', 'requires_license', 'license'])
+    df.drop(columns=['host_location', 'host_is_superhost', 'city', 'requires_license', 'license'], axis=1, inplace=True)
+    return df
 
+def add_violation_col(df):
+    '''
+    Desc.
+    
+    Parameters
+    ----------
+    df: 
+    cols: 
 
-# def save(df):
-#      df.to_pickle('../data/pickled_listings_df')
+    Returns
+    ----------
+    df: 
+    '''
+    df['is_violating'] = df['listing_url'].map(lambda x: 1.0 if x in violater else 0.0) 
+    return df
+
+def save(df):
+     df.to_pickle('../data/pickled_listings_df')
 
 if __name__ == '__main__':
     df = pd.read_csv('../data/listings.csv')
@@ -237,7 +268,56 @@ if __name__ == '__main__':
     ### STANDARDIZING START
 
     text_cols = ['summary', 'space', 'description', 'notes', 'access', 'interaction', 'house_rules', 'host_about']
-    drop_cols = ['host_location', 'host_is_superhost', 'city', 'requires_license', 'license']
+    violater = ['https://www.airbnb.com/rooms/2086', 'https://www.airbnb.com/rooms/36026536',
+                'https://www.airbnb.com/rooms/30991941', 'https://www.airbnb.com/rooms/36110171', 
+                'https://www.airbnb.com/rooms/283162', 'https://www.airbnb.com/rooms/21281617', 
+                'https://www.airbnb.com/rooms/27987428', 'https://www.airbnb.com/rooms/2139342',
+                'https://www.airbnb.com/rooms/1488774', 'https://www.airbnb.com/rooms/1153002',
+                'https://www.airbnb.com/rooms/2599115', 'https://www.airbnb.com/rooms/2827887',
+                'https://www.airbnb.com/rooms/3495498', 'https://www.airbnb.com/rooms/4137490',
+                'https://www.airbnb.com/rooms/4556950', 'https://www.airbnb.com/rooms/4671735',
+                'https://www.airbnb.com/rooms/19214092', 'https://www.airbnb.com/rooms/4753876',
+                'https://www.airbnb.com/rooms/4896381', 'https://www.airbnb.com/rooms/5070640',
+                'https://www.airbnb.com/rooms/5402378', 'https://www.airbnb.com/rooms/35678320',
+                'https://www.airbnb.com/rooms/35683218', 'https://www.airbnb.com/rooms/35828919',
+                'https://www.airbnb.com/rooms/21753893', 'https://www.airbnb.com/rooms/5696654',
+                'https://www.airbnb.com/rooms/8797683', 'https://www.airbnb.com/rooms/37746342',
+                'https://www.airbnb.com/rooms/6288460', 'https://www.airbnb.com/rooms/6370140',
+                'https://www.airbnb.com/rooms/7475742', 'https://www.airbnb.com/rooms/7859145',
+                'https://www.airbnb.com/rooms/8211278', 'https://www.airbnb.com/rooms/8366762',
+                'https://www.airbnb.com/rooms/8555795', 'https://www.airbnb.com/rooms/8739814',
+                'https://www.airbnb.com/rooms/8829680', 'https://www.airbnb.com/rooms/8884899',
+                'https://www.airbnb.com/rooms/8951180', 'https://www.airbnb.com/rooms/8989473',
+                'https://www.airbnb.com/rooms/9165337', 'https://www.airbnb.com/rooms/20419783',
+                'https://www.airbnb.com/rooms/9372481', 'https://www.airbnb.com/rooms/9591731',
+                'https://www.airbnb.com/rooms/9169634', 'https://www.airbnb.com/rooms/10426535',
+                'https://www.airbnb.com/rooms/10088031', 'https://www.airbnb.com/rooms/9237825',
+                'https://www.airbnb.com/rooms/9330036', 'https://www.airbnb.com/rooms/9409334',
+                'https://www.airbnb.com/rooms/9532575', 'https://www.airbnb.com/rooms/12172692',
+                'https://www.airbnb.com/rooms/11377981', 'https://www.airbnb.com/rooms/21899331',
+                'https://www.airbnb.com/rooms/11358422', 'https://www.airbnb.com/rooms/11240044',
+                'https://www.airbnb.com/rooms/11191271', 'https://www.airbnb.com/rooms/11148272',
+                'https://www.airbnb.com/rooms/11070296', 'https://www.airbnb.com/rooms/10995273',
+                'https://www.airbnb.com/rooms/15743145', 'https://www.airbnb.com/rooms/10706175',
+                'https://www.airbnb.com/rooms/10392285', 'https://www.airbnb.com/rooms/18000300',
+                'https://www.airbnb.com/rooms/10190798', 'https://www.airbnb.com/rooms/9856869',
+                'https://www.airbnb.com/rooms/9796646', 'https://www.airbnb.com/rooms/9795040',
+                'https://www.airbnb.com/rooms/22307026', 'https://www.airbnb.com/rooms/9734548',
+                'https://www.airbnb.com/rooms/9633450', 'https://www.airbnb.com/rooms/12711245',
+                'https://www.airbnb.com/rooms/12681403', 'https://www.airbnb.com/rooms/12791280',
+                'https://www.airbnb.com/rooms/13550337', 'https://www.airbnb.com/rooms/13377190',
+                'https://www.airbnb.com/rooms/13140326', 'https://www.airbnb.com/rooms/12874653',
+                'https://www.airbnb.com/rooms/13688138', 'https://www.airbnb.com/rooms/13656290',
+                'https://www.airbnb.com/rooms/13877553', 'https://www.airbnb.com/rooms/14654146',
+                'https://www.airbnb.com/rooms/14902544', 'https://www.airbnb.com/rooms/15097005',
+                'https://www.airbnb.com/rooms/15237689', 'https://www.airbnb.com/rooms/38982793',
+                'https://www.airbnb.com/rooms/19141160', 'https://www.airbnb.com/rooms/15583685',
+                'https://www.airbnb.com/rooms/15585225', 'https://www.airbnb.com/rooms/15641776',
+                'https://www.airbnb.com/rooms/15680276', 'https://www.airbnb.com/rooms/15745694',
+                'https://www.airbnb.com/rooms/15807599', 'https://www.airbnb.com/rooms/27545807',
+                'https://www.airbnb.com/rooms/14125469', 'https://www.airbnb.com/rooms/16497996',
+                'https://www.airbnb.com/rooms/16443175', 'https://www.airbnb.com/rooms/16392236',
+                'https://www.airbnb.com/rooms/16299372']
 
     df = NaN_to_None(df, text_cols)
     df = host_in_Denver(df)
@@ -245,16 +325,16 @@ if __name__ == '__main__':
     df = in_top_10_neighbourhood(df)
     df = listing_location(df)
     df = fill_NaN_pricing(df)
+    df = NaN_to_zero(df)
     df = room_type_dummies(df)
     df = current_license(df)
-    df = drop_cols(df, drop_cols)
+    df = drop_cols(df)
+    df = add_violation_col(df)
 
     ### STANDARDIZING END
+    save(df)
 
 
-
-
-    # save(df)
 
 
 
